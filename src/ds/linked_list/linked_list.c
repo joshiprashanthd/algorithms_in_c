754db11d8fda67242b3b6fdb89e2f463b5870ef2
joshiprashanthd <joshiprashanthd121@gmail.com>
2020-12-25 13:59:08 +0530

    feat: add Linked List implementation

---
+++ src/ds/linked_list/linked_list.c
@@ -0,0 +1,61 @@
+#include "linked_list.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void insert(struct Node** base_node, char* value) {
+  struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+  newNode->value = value;
+
+  if ((*base_node) == NULL) {
+    *base_node = newNode;
+  } else {
+    struct Node* temp = *base_node;
+    while (temp->next != NULL) temp = temp->next;
+    temp->next = newNode;
+    temp = NULL;
+  }
+}
+
+int search(struct Node* base_node, char* value) {
+  struct Node* temp = base_node;
+
+  while (temp != NULL) {
+    if (temp->value == value) return 1;
+    temp = temp->next;
+  }
+
+  return 0;
+}
+
+int delete (struct Node** base_node, char* value) {
+  assert(*base_node != NULL);
+
+  int result = search(*base_node, value);
+
+  if (result != 1) return 0;
+
+  struct Node* temp = *base_node;
+
+  // temp points to previous node of the node to remove
+  while ((temp->next)->value != value) temp = temp->next;
+
+  struct Node* node = temp->next;
+  temp->next = node->next;
+
+  free(node);
+
+  return 1;
+}
+
+void show(struct Node* base_node) {
+  struct Node* temp = base_node;
+
+  while (temp->next != NULL) {
+    printf("[%s] -> ", temp->value);
+    temp = temp->next;
+  }
+
+  printf("[%s]\n", temp->value);
+}
---
+++ src/ds/linked_list/linked_list.h
@@ -0,0 +1,18 @@
+#ifndef linked_list_h
+#define linked_list_h
+
+struct Node {
+  char* value;
+  struct Node* next;
+};
+
+struct LinkedList {
+  int count;
+  struct Node* base_node;
+};
+
+void insert(struct Node** base_node, char* value);
+int search(struct Node* base_node, char* value);
+int delete (struct Node** base_node, char* value);
+void show(struct Node* base_node);
+#endif
