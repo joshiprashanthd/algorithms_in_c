21753a3e5c6739e2d41936dd079349d098c53d36
joshiprashanthd <joshiprashanthd121@gmail.com>
2021-01-02 20:52:33 +0530

    refactor: implement oop design in linked list header file
    all function name are prefixed by the type of data structure they use.
    
    In case of linked lists all function names are prefixed by `linked_list_`
    
    A new function `linked_list_new` is added to return pointer to the linkedlist instance

--- src/ds/linked_list/linked_list.c
+++ src/ds/linked_list/linked_list.c
@@ -4,27 +4,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int append(struct Node** root, char* value) {
-  if (value == NULL) return 0;
+struct LinkedList* linked_list_new() {
+  struct LinkedList* ll = (struct LinkedList*)malloc(sizeof(struct LinkedList));
+  ll->count = 0;
+  ll->root = NULL;
+  return ll;
+}
 
-  struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+int linked_list_append(struct LinkedList** linked_list, char* value) {
+  assert(value != NULL);
+
+  struct LinkedListNode* new_node =
+      (struct LinkedListNode*)malloc(sizeof(struct LinkedListNode));
   new_node->value = value;
   new_node->next = NULL;
 
-  if ((*root) == NULL) {
-    *root = new_node;
+  if ((*linked_list)->root == NULL) {
+    (*linked_list)->root = new_node;
   } else {
-    struct Node* temp = *root;
+    struct LinkedListNode* temp = (*linked_list)->root;
     while (temp->next != NULL) temp = temp->next;
     temp->next = new_node;
     temp = NULL;
   }
 
+  (*linked_list)->count++;
+
   return 1;
 }
 
-int search(struct Node* root, char* value) {
-  struct Node* temp = root;
+int linked_list_search(struct LinkedList* linked_list, char* value) {
+  assert(value != NULL);
+
+  struct LinkedListNode* temp = linked_list->root;
 
   while (temp != NULL) {
     if (temp->value == value) return 1;
@@ -34,28 +46,29 @@ int search(struct Node* root, char* value) {
   return 0;
 }
 
-int delete (struct Node** root, char* value) {
-  assert(*root != NULL);
+int linked_list_delete(struct LinkedList** linked_list, char* value) {
+  assert((*linked_list)->root != NULL);
 
-  int result = search(*root, value);
+  int result = linked_list_search(*linked_list, value);
 
   if (result != 1) return 0;
 
-  struct Node* temp = *root;
+  struct LinkedListNode* temp = (*linked_list)->root;
 
   // temp points to previous node of the node to remove
   while ((temp->next)->value != value) temp = temp->next;
 
-  struct Node* node = temp->next;
+  struct LinkedListNode* node = temp->next;
   temp->next = node->next;
-
   free(node);
+
+  (*linked_list)->count--;
 
   return 1;
 }
 
-void show(struct Node* root) {
-  struct Node* temp = root;
+void linked_list_show(struct LinkedList* linked_list) {
+  struct LinkedListNode* temp = linked_list->root;
 
   while (temp->next != NULL) {
     printf("[%s] -> ", temp->value);
@@ -65,21 +78,25 @@ void show(struct Node* root) {
   printf("[%s]\n", temp->value);
 }
 
-char* pop(struct Node** root) {
-  if ((*root) == NULL || root == NULL) return NULL;
-  if ((*root)->next == NULL) {
-    char* target_value = (*root)->value;
-    *root = NULL;
+char* linked_list_pop(struct LinkedList** linked_list) {
+  struct LinkedListNode* root = (*linked_list)->root;
+  if (root == NULL) return NULL;
+  if (root->next == NULL) {
+    char* target_value = root->value;
+    (*linked_list)->root = NULL;
+    (*linked_list)->count = 0;
     return target_value;
   }
 
-  struct Node* temp = *root;
+  struct LinkedListNode* temp = root;
   while ((temp->next)->next != NULL) temp = temp->next;
 
-  struct Node* target = temp->next;
-  char* target_value = (temp->next)->value;
+  struct LinkedListNode* target_node = temp->next;
+  char* target_value = target_node->value;
   temp->next = NULL;
-  free(target);
+  free(target_node);
+
+  (*linked_list)->count--;
 
   return target_value;
 }
@@ -87,25 +104,24 @@ char* pop(struct Node** root) {
 void test_linked_list() {
   printf("Linked List Test started\n");
 
-  struct LinkedList ll = {0, NULL};
+  struct LinkedList* ll = linked_list_new();
 
-  assert(append(&ll.root, "A") == 1);
-  assert(append(&ll.root, "B") == 1);
-  assert(append(&ll.root, NULL) == 0);
+  assert(linked_list_append(&ll, "A") == 1);
+  assert(linked_list_append(&ll, "B") == 1);
   printf("\tTest `append` completed\n");
 
-  assert(search(ll.root, "A") == 1);
-  assert(search(ll.root, "B") == 1);
-  assert(search(ll.root, "9") == 0);
+  assert(linked_list_search(ll, "A") == 1);
+  assert(linked_list_search(ll, "B") == 1);
+  assert(linked_list_search(ll, "9") == 0);
   printf("\tTest `search` completed\n");
 
-  assert(delete (&ll.root, "B") == 1);
-  assert(search(ll.root, "B") == 0);
+  assert(linked_list_delete(&ll, "B") == 1);
+  assert(linked_list_search(ll, "B") == 0);
   printf("\tTest `delete` completed\n");
 
-  append(&ll.root, "B");
-  assert(pop(&ll.root) == "B");
-  assert(pop(&ll.root) == "A");
+  linked_list_append(&ll, "B");
+  assert(linked_list_pop(&ll) == "B");
+  assert(linked_list_pop(&ll) == "A");
   printf("\tTest `pop` completed\n");
 
   printf("Linked List Test Completed");
--- src/ds/linked_list/linked_list.h
+++ src/ds/linked_list/linked_list.h
@@ -1,20 +1,21 @@
 #ifndef linked_list_h
 #define linked_list_h
 
-struct Node {
+struct LinkedListNode {
   char* value;
-  struct Node* next;
+  struct LinkedListNode* next;
 };
 
 struct LinkedList {
   int count;
-  struct Node* root;
+  struct LinkedListNode* root;
 };
 
-int append(struct Node** root, char* value);
-int search(struct Node* root, char* value);
-int delete (struct Node** root, char* value);
-char* pop(struct Node** root);
-void show(struct Node* root);
+struct LinkedList* linked_list_new();
+int linked_list_append(struct LinkedList** linked_list, char* value);
+int linked_list_search(struct LinkedList* linked_list, char* value);
+int linked_list_delete(struct LinkedList** linked_list, char* value);
+char* linked_list_pop(struct LinkedList** linked_list);
+void linked_list_show(struct LinkedList* linked_list);
 void test_linked_list();
 #endif
